f6ff9711198e321233633d74bfe3a515e3c15ca3
Arturo Valdez <santiguardado27@gmail.com>
2025-03-03 16:20:27 -0600

    Creacion de Clase Cliente Servicio

--- src/main/java/gm/zona_fit/repositorio/ClienteRepositorio.java
+++ src/main/java/gm/zona_fit/repositorio/ClienteRepositorio.java
@@ -4,6 +4,4 @@ import gm.zona_fit.modelo.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 //Tipo de dato y clave de llave primaria
 public interface ClienteRepositorio extends JpaRepository<Cliente, Integer> {
-
-
 }
