0a7fa63c35f89b3ae79193816bf6befd38a99293
Arturo Valdez <santiguardado27@gmail.com>
2025-03-03 15:58:21 -0600

    Creacion de la Clase Entidad de Cliente con Sping

---
+++ src/main/java/gm/zona_fit/modelo/Cliente.java
@@ -0,0 +1,28 @@
+package gm.zona_fit.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+//Se utilizan objetos en vez de tipos primitivos
+@Entity
+//Generador de metodos get y set con la anotacion
+@Data
+//Generador de constructor con ningun argumento
+@NoArgsConstructor
+//Generador de constructor con todos los argumento
+@AllArgsConstructor
+//Generador del toString
+@ToString
+//Generador de Equials y HashCode que nos agiliza la busqueda
+@EqualsAndHashCode
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nombre;
+    private String apellido;
+    private Integer membresia;
+}
